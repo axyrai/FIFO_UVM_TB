08d85ee6471595ca3d06f289b081d4a2a840889e
Akshay Rai <rai.axay@gmail.com>
2023-09-13 10:20:56 +0530

    modified monitor

--- fifo_monitor.sv
+++ fifo_monitor.sv
@@ -20,7 +20,28 @@ class fifo_monitor extends uvm_monitor;
     forever begin
       @(posedge vif.m_mp.clk)
       if(vif.m_mp.m_cb.i_wren==1)
-
+        begin
+		packet.i_wrdata = vif.m_mp.m_cb.i_wrdata;
+        packet.i_wren = 'b1;
+        packet.i_rden = 'b0;
+        packet.o_full = vif.m_mp.m_cb.o_full;
+        packet.o_empty=vif.m_mp.m_cb.o_empty;
+        packet.o_alm_full=vif.m_mp.m_cb.o_alm_empty;
+        packet.o_alm_empty=vif.m_mp.m_cb.o_alm_empty; 
+        packet_port.write(packet);
+        end
+      else if(vif.m_mp.m_cb.i_rden == 1)
+        begin
+        packet.o_rddata = vif.m_mp.m_cb.o_rddata;
+        packet.i_wren = 'b0;
+        packet.i_rden = 'b1;
+        packet.o_full = vif.m_mp.m_cb.o_full;
+        packet.o_empty=vif.m_mp.m_cb.o_empty;
+        packet.o_alm_full=vif.m_mp.m_cb.o_alm_empty;
+        packet.o_alm_empty=vif.m_mp.m_cb.o_alm_empty; 
+        packet_port.write(packet);
+        end
       endtask
     endclass
+
 
